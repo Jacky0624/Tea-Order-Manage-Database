01265e3b90c6006a35174f05a9f6e2f415b3526c
Jacky0624 <a0987143311@gmail.com>
2025-03-14 16:55:45 +0800

    add select column OrderDate

--- Procedures/GetAllOrders.sql
+++ Procedures/GetAllOrders.sql
@@ -2,6 +2,6 @@ CREATE PROCEDURE GetAllOrders
 AS
 BEGIN
     SET NOCOUNT ON;
-    SELECT Id, Phone, Title, Address, OrderStatus, TotalAmount, CreateAt, CreateUser, ModifyAt, ModifyUser, Remark
+    SELECT Id, Phone, Title, Address, OrderStatus, TotalAmount, CreateAt, CreateUser, ModifyAt, ModifyUser, Remark, OrderDate
     FROM Orders;
 END;
