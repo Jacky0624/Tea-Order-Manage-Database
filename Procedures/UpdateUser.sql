ff163aaaacfe2989917f1c59eb9b793ef1682383
Jacky0624 <a0987143311@gmail.com>
2025-03-17 09:19:51 +0800

    modify updateUser

--- Procedures/UpdateUser.sql
+++ Procedures/UpdateUser.sql
@@ -4,13 +4,14 @@ CREATE PROCEDURE UpdateUser
     @Account NVARCHAR(100),
     @RoleId INT,
     @ModifyAt DATETIME,
-    @ModifyUser NVARCHAR(100)
+    @ModifyUser NVARCHAR(100),
+    @PasswordHash NVARCHAR(255),
 AS
 BEGIN
     SET NOCOUNT ON;
 
     UPDATE Users
     SET Username = @Username, Account = @Account, RoleId = @RoleId, 
-        ModifyAt = @ModifyAt, ModifyUser = @ModifyUser
+        ModifyAt = @ModifyAt, ModifyUser = @ModifyUser, PasswordHash = @PasswordHash
     WHERE Id = @Id AND IsDeleted = 0;
 END;
